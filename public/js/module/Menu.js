e11c842a53ec344d05ba4769abe4a58b43fac900
Tomasz <tborycki@yahoo.pl>
2016-11-14 16:07:39 +0100

    electron test!

--- public/js/module/Menu.js
+++ public/js/module/Menu.js
@@ -18,9 +18,16 @@ define([
 		
 			//this.add.button("Graj", 500, 300, 300, 80, '#077607', '#0cd70c', '#333', null);
 			//this.add.button("Opcje", 500, 410, 300, 80, '#077607', '#0cd70c', '#333', null);
-			this.add.buttonImg("main", 'graj', 'graj-hover', 550, 280, 0,0, that.play);	
+			// this.add.buttonImg("main", 'graj', 'graj-hover', 550, 280, 0,0, that.play);	
 
-			this.add.buttonImg("main", 'opcje', 'opcje-hover', 530, 430, 0,0, null);	
+			// this.add.buttonImg("main", 'opcje', 'opcje-hover', 530, 430, 0,0, null);
+
+			this.add.button('Graj',600, 310 , 180, 60, null, null, 'black', 'green', '#333', that.play);
+			this.add.button('Opcje',600, 400 , 180, 60, null, null, 'black', 'green', '#333', null);
+			this.add.button('Zamknij',600, 490 , 180, 60, null, null, 'black', 'green', '#333', window.close);
+           
+				
+			
 	
 			// that.img = this.add.image('main', this.rand(20,800),this.rand(20,600),'menu-bg',80, 80);
 			// var img2 = this.add.image('main', this.rand(20,800),this.rand(20,800),'menu-bg',80, 80);
